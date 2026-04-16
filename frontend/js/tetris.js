0e9109c63196ba6e278862625490a2223dedaaee
Xiang1218-Yu <1061366940@qq.com>
2026-04-16 15:34:08 +0800

    fix

--- frontend/js/tetris.js
+++ frontend/js/tetris.js
@@ -145,6 +145,7 @@ class TetrisGame {
       this.BLOCK_SIZE = newBlockSize;
       this.canvas.width = this.COLS * newBlockSize;
       this.canvas.height = this.ROWS * newBlockSize;
+      this.bindEvents();
       this.draw();
     }
   }
